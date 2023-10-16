88bb4a04025326eef9d606aa24905c3f67298a92
lonewanderer27 <adrianejames03@gmail.com>
2023-10-17 06:32:14 +0800

    complete app

---
+++ app/src/main/java/com/zy/snakesladders/Zy_SnL_MainActivity.java
@@ -0,0 +1,106 @@
+package com.zy.snakesladders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Zy_SnL_MainActivity extends AppCompatActivity {
+    ImageView Dice;
+    Button Play, Reset;
+    int DiceValue;
+    int CurrentPos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zy_snl_activity_main);
+
+        Dice = findViewById(R.id.dice);
+        Play = findViewById(R.id.playBtn);
+        Reset = findViewById(R.id.resetBtn);
+
+        Play.setOnClickListener(v -> {
+            Play();
+        });
+        Reset.setOnClickListener(v -> {
+            Reset();
+        });
+    }
+
+    void Reset() {
+        DiceValue = 0;
+        CurrentPos = 0;
+        UpdateDice();
+        UpdateBoxes();
+    }
+
+    void Play() {
+        DiceValue = (int) (Math.random() * 6) + 1;
+        Log.i("Dice", "Rolled: " + DiceValue);
+        UpdateDice();
+
+        CurrentPos += DiceValue;
+
+        if (CurrentPos > 60) {
+            int excess = CurrentPos - 60;
+            CurrentPos = 60 - excess;
+        }
+
+        if (CurrentPos == 60) {
+            PlayFinishAudio();
+        } else {
+            PlayTransitionAudio();
+        }
+
+        UpdateBoxes();
+    }
+
+    void UpdateBoxes() {
+        for (int i = 1; i < 61; i++) {
+            int id = getResources().getIdentifier("box" + i, "id", getPackageName());
+            TextView box = findViewById(id);
+
+            if (i == CurrentPos) {
+                box.setBackgroundResource(R.drawable.selected_border_color);
+            } else {
+                box.setBackgroundResource(R.drawable.border_color);
+            }
+        }
+    }
+
+    void UpdateDice() {
+        switch (DiceValue) {
+            case 1:
+                Dice.setBackgroundResource(R.drawable.dice_z1);
+                break;
+            case 2:
+                Dice.setBackgroundResource(R.drawable.dice_z2);
+                break;
+            case 3:
+                Dice.setBackgroundResource(R.drawable.dice_z3);
+                break;
+            case 4:
+                Dice.setBackgroundResource(R.drawable.dice_z4);
+                break;
+            case 5:
+                Dice.setBackgroundResource(R.drawable.dice_z5);
+                break;
+            case 6:
+                Dice.setBackgroundResource(R.drawable.dice_z6);
+                break;
+        }
+    }
+
+    void PlayTransitionAudio() {
+        MediaPlayer.create(this, R.raw.transition).start();
+    }
+
+    void PlayFinishAudio() {
+        MediaPlayer.create(this, R.raw.finish).start();
+    }
+}
